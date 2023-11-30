35a6f6d9720d8438a7b9b39cc268c6d5cd019fc3
antoninchafiol <antonin.chafiol@gmail.com>
2023-11-30 01:39:36 +0100

    [UPDT] - FE - Change to match only 1 dataframe - Still some issues

--- src/data_prep/feature_engineering.py
+++ src/data_prep/feature_engineering.py
@@ -2,21 +2,50 @@ import pandas as pd
 from sklearn.cluster._kmeans import KMeans
 from sklearn.decomposition import PCA
 
-# Adding simple kmeans
+class FeatureTrain():
+    def __init__(self, features_df, labels_df):
+        self.features = features_df
+        self.labels = labels_df
+        self.X = pd.DataFrame()
+    def merge(self):
+        l = self.labels.drop(columns='night')
+        l['event'] = self.labels['event'].apply(lambda x: 1 if x=="onset" else 2)
+        l['step'] = l['step'].astype("int32")
+        self.X = self.features.merge(l,how='left', on=["series_id","step"]).fillna(0)
+        self.X['event'] = self.X['event'].astype("int32")
+        return self
 
-# Adding some PCA
+    def apply_kmeans(self):
+        km = KMeans(n_clusters=2, n_init=5, max_iter=55)
+        self.X['cluster'] = km.fit(self.X.drop(columns=['series_id', 'step', 'event'])).labels_
+        return self
+    def apply_PCA(self):
+        pca = PCA()
+        # print(self.features)
+        features_PCA = pca.fit_transform(self.X.drop(columns=['series_id', 'step', 'event']))
+        features_PCA = pd.DataFrame(features_PCA, columns=[f"PC{i+1}" for i in range(features_PCA.shape[1])])
+        self.X = self.X.merge(features_PCA, left_index=True, right_index=True)
+        return self
+    def apply_mult(self):
+        self.X['mult'] = self.X['anglez'] * self.X['enmo']
+        return self
+    def apply_lag(self, lag_nb):
+        for i in range(1, lag_nb+1):
+            self.X[f'lag_{i}_anglez'] = self.X['anglez'].shift(i)
+            self.X[f'lag_{i}_enmo'] = self.X['enmo'].shift(i)
+        self.X = self.X.dropna()
+        return self
 
-# Getting some rolling average
+    def save(self, path_str):
+        self.X.to_parquet(f"data/custom/X_fe_{path_str}.parquet")
+    def fit(self):
+        self.X = self.apply_PCA()
+        self.X = self.apply_kmeans()
+        self.X = self.apply_mult()
+        self.X = self.apply_lag(2)
+        return self
 
-# Mult anglez with enmo
-
-# Adding some lag of both features
-
-# Create a class that group it all
-
-# 
-
-class Feature_eng():
+class FeatureEng():
     features = pd.DataFrame()
     labels = pd.DataFrame()
     def __init__(self, features_path, labels_path):
@@ -24,25 +53,44 @@ class Feature_eng():
         self.labels = labels_path
     def apply_kmeans(self):
         km = KMeans(n_clusters=2, n_init=5, max_iter=55)
-        self.features['cluster'] = km.fit(self.features[["step", "anglez", "enmo"]].labels)
+        self.features['cluster'] = km.fit(self.features[["step", "anglez", "enmo"]]).labels_
         return self
     def apply_PCA(self):
         pca = PCA()
-        features_PCA = pca.fit_transform(self.features)
+        # print(self.features)
+        features_PCA = pca.fit_transform(self.features.drop(columns=['series_id', 'step']))
         features_PCA = pd.DataFrame(features_PCA, columns=[f"PC{i+1}" for i in range(features_PCA.shape[1])])
         self.features = self.features.merge(features_PCA, left_index=True, right_index=True)
         return self
     def apply_rolling_average(self):
         return self
+    def apply_mult(self):
+        self.features['mult'] = self.features['anglez'] * self.features['enmo']
+        return self
     def apply_lag(self, lag_nb):
-        for i in range(lag_nb):
+        for i in range(1, lag_nb+1):
             self.features[f'lag_{i}_anglez'] = self.features['anglez'].shift(i)
             self.features[f'lag_{i}_enmo'] = self.features['enmo'].shift(i)
+        self.features = self.features.dropna()
         return self
     def apply_label_conversion(self):
         self.labels['digit_event'] = self.labels['event'].apply(lambda x: 0 if x=="onset" else 1)
         return self
-    
+    def save(self, path_str):
+        self.labels.to_parquet(f"data/custom/event_fe_{path_str}.parquet")
+        self.features.to_parquet(f"data/custom/series_fe_{path_str}.parquet")
+    def fit(self):
+        return self.apply_PCA().apply_kmeans().apply_mult().apply_lag(2).apply_label_conversion()
 
-    if __name__=="__main__":
-        fe = Feature_eng(pd.read_parquet("data/custom/series_dropna_timestamp.parquet"), pd.read_parquet("data/custom/event_dropna_timestamp.parquet"))
+if __name__=="__main__":
+    # fe = FeatureEng(pd.read_parquet("data/custom/series_dropna_timestamp.parquet"), pd.read_parquet("data/custom/event_dropna_timestamp.parquet"))
+    # fee = fe.fit()
+    # print(fee.features)
+    # fee.save("0_basic_noroll")
+
+    fe = FeatureTrain(pd.read_parquet("data/custom/series_dropna_timestamp.parquet"), pd.read_parquet("data/custom/event_dropna_timestamp.parquet"))
+    fe = fe.fit()
+    fe.save()
+
+    print(fe.X)
+    print(fe.X['event'].unique())
---
+++ src/models/models_training.py
@@ -0,0 +1,20 @@
+# Sklearn's definitions
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+
+
+from xgboost import XGBClassifier
+
+
+
+# Pytorch's definitions
+
+from models_definition import *
+
+from ..feature_engineering import *
+
+
+def output_processing():
+    pass
